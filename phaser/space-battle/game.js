0ef6642fd9e62c5beb33fcc8e74e2ec19bdc374e
michele dibenedetto <michele.dibe78@gmail.com>
2018-07-20 22:28:44 +0100

    in progress

--- phaser/space-battle/game.js
+++ phaser/space-battle/game.js
@@ -112,15 +112,16 @@ function createEnemies() {
         for (var x = 0; x < 10; x++) {
             var enemy = enemies.create(x * 48, y * 50, 'enemy');
             enemy.anchor.setTo(0.5, 0.5);
+            enemy.body.moves = false;
         }
     }
 
     enemies.x = 100;
     enemies.y = 50;
 
-    // var tween = game.add.tween(enemies)
-    //     .to({ x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
-    // tween.onLoop.add(descend, this);
+    var tween = game.add.tween(enemies)
+        .to({ x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+    tween.onLoop.add(descend, this);
 }
 
 function descend() {
