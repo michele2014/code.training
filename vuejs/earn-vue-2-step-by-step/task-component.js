1eb4ec6e3838c2b5e68f8fb57ecd4f18d5d0fd53
michele dibenedetto <michele.dibenedetto@hmhco.com>
2018-08-31 17:18:38 +0100

    components

---
+++ vuejs/earn-vue-2-step-by-step/task-component.js
@@ -0,0 +1,26 @@
+Vue.component('task-list', {
+    template: `<div>
+                    <task v-for = "task in tasks" >{{ task.task }}</task>
+                </div>`,
+    data() {
+        return {
+            tasks: [
+                { task: 'Go to the store', comleted: true },
+                { task: 'Go to the bank', comleted: false },
+                { task: 'Go to the farm', comleted: true },
+                { task: 'Go to work', comleted: false }
+            ]
+        }
+    }
+});
+
+
+
+Vue.component('task', {
+    template: '<li><slot></slot></li>',
+    data() {
+        return {
+            message: 'football'
+        }
+    }
+});
