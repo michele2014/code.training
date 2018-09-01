93f82bdb422d439cda08069888c7edbf5b536c35
michele dibenedetto <michele.dibe78@gmail.com>
2018-09-01 18:25:42 +0100

    [unity] - add movements character

---
+++ unity/Fundamentals2D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D _rigidBody;
+    SpriteRenderer _renderer;
+    Animator _animator;
+
+    public float maxSpeed;
+    bool facingRight = true;
+
+	// Use this for initialization
+	void Start () {
+        this._rigidBody = GetComponent<Rigidbody2D>();
+        this._renderer = GetComponent<SpriteRenderer>();
+        this._animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var move = Input.GetAxis("Horizontal");
+
+        if(move > 0 && !facingRight){
+            Flip();
+        }
+        else if (move < 0 && facingRight)
+        {
+            Flip();
+        }
+
+        this._rigidBody.velocity = new Vector2(move * maxSpeed, this._rigidBody.velocity.y);
+        this._animator.SetFloat("MoveSpeed",Mathf.Abs(move));
+	}
+
+    void Flip(){
+        facingRight = !facingRight;
+        this._renderer.flipX = !this._renderer.flipX;
+    }
+}
