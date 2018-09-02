21ab90f2d4345bddb623cd85a04f19761fee8068
michele dibenedetto <michele.dibe78@gmail.com>
2018-09-02 20:09:24 +0100

    added all animations

--- unity/Fundamentals2D/Assets/Scripts/PlayerController.cs
+++ unity/Fundamentals2D/Assets/Scripts/PlayerController.cs
@@ -8,8 +8,17 @@ public class PlayerController : MonoBehaviour {
     SpriteRenderer _renderer;
     Animator _animator;
 
-    public float maxSpeed;
+    bool canMove = true;
     bool facingRight = true;
+    //move
+    public float maxSpeed;
+    //jump
+    bool grounded = false;
+    float groundCheckRadius = 0.2f;
+
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float jumpPower;
 
 	// Use this for initialization
 	void Start () {
@@ -20,22 +29,45 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //JUMP
+        if(canMove && grounded && Input.GetAxis("Jump") > 0){
+            this._animator.SetBool("IsGrounded", false);
+            this._rigidBody.velocity = new Vector2(this._rigidBody.velocity.x, 0f);
+            this._rigidBody.AddForce(new Vector2(0f, jumpPower), ForceMode2D.Impulse);
+            grounded = false;
+        }
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        this._animator.SetBool("IsGrounded", grounded);
+
+        //MOVE
         var move = Input.GetAxis("Horizontal");
 
-        if(move > 0 && !facingRight){
-            Flip();
-        }
-        else if (move < 0 && facingRight)
-        {
-            Flip();
-        }
+        if(canMove){
+            if (move > 0 && !facingRight)
+            {
+                Flip();
+            }
+            else if (move < 0 && facingRight)
+            {
+                Flip();
+            }
 
-        this._rigidBody.velocity = new Vector2(move * maxSpeed, this._rigidBody.velocity.y);
-        this._animator.SetFloat("MoveSpeed",Mathf.Abs(move));
+            this._rigidBody.velocity = new Vector2(move * maxSpeed, this._rigidBody.velocity.y);
+            this._animator.SetFloat("MoveSpeed", Mathf.Abs(move));
+        }
+        else{
+            this._rigidBody.velocity = new Vector2(0, this._rigidBody.velocity.y);
+            this._animator.SetFloat("MoveSpeed", 0);
+        }
 	}
 
     void Flip(){
         facingRight = !facingRight;
         this._renderer.flipX = !this._renderer.flipX;
+    }
+
+    public void ToggleCanMove(){
+        this.canMove = !this.canMove;
     }
 }
