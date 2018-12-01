7cfe942708a2aa812b9d7065683a7c2d52bebaa1
michele dibenedetto <michele.dibe78@gmail.com>
2018-12-01 22:46:45 +0000

    clean up

--- dcu/assignment-1/js/script.js
+++ dcu/assignment-1/js/script.js
@@ -4,24 +4,9 @@ document.addEventListener('DOMContentLoaded', function () {
     link.classList.add('active');
 }, false);
 
-// function loadMenu() {
-//     const menuItems = 
-//                 `<ul>
-//                     <li><a onclick="setActiveLink(this)" href="index.html" class="active">Home</a></li>
-//                     <li><a onclick="setActiveLink(this)" href="contact.html">Contact</a></li>
-//                     <li><a onclick="setActiveLink(this)" href="about.html">About</a></li>
-//                 </ul>`;
-//     const menuElement = document.querySelector('.menu');
-//     menuElement.innerHTML = menuItems;
-// }
-
-
 function setActiveLink(selectedLink) {
     var links = document.querySelectorAll('a');
     links.forEach(link => link.classList.remove('active'));
     selectedLink.classList.add('active');
-
-    console.log(selectedLink)
-
     return false;
 }
